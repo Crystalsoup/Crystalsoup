90bc4177cf85ae35fcd8dd36dcd8f98688b147b6
Crystal <113200390+Crystalsoup@users.noreply.github.com>
2022-09-14 22:11:14 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace WhatTheFuckMate
+{
+
+    class Bruh
+    {
+
+        Bruh(string urmom)
+        {
+            Console.WriteLine($"urmom: {urmom}");
+        }
+
+        Bruh(int weight)
+        {
+            Console.WriteLine($"weight: {weight}");
+        
+        }
+       
+        static void Main(string[] args) {
+
+            Bruh bruh = new Bruh("Yo mama so fat");
+
+            Console.WriteLine();
+
+            Bruh bruh2 = new Bruh(700);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
+
+
